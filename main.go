5be0e0eaf63de0ca5e778dfdc5f757e1aecdd14d
dan2k <sangtong@gmail.com>
2023-05-02 13:13:07 +0700

    add log and add mac address

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
-	"runtime"
 	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"net"
 	"net/http"
 	"os"
-	"time"
-	"github.com/spf13/viper"
+	"os/exec"
+	"runtime"
 	"strings"
+	"time"
+
+	"github.com/spf13/viper"
 )
 
 type Response struct {
@@ -19,9 +21,10 @@ type Response struct {
 }
 
 func main() {
+
 	viper.SetConfigName("config") // ชื่อ config file
-	viper.AddConfigPath(".") // ระบุ path ของ config file
-	viper.AutomaticEnv() // อ่าน value จาก ENV variable
+	viper.AddConfigPath(".")      // ระบุ path ของ config file
+	viper.AutomaticEnv()          // อ่าน value จาก ENV variable
 	// แปลง _ underscore ใน env เป็น . dot notation ใน viper
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	// อ่าน config
@@ -29,27 +32,43 @@ func main() {
 	if err != nil {
 		panic(fmt.Errorf("fatal error config file: %s ", err))
 	}
-    ipAddr:=getIp()
-	mode:=viper.GetString("app.mode")
-	var api string
-	if mode=="developement" {
-		api = fmt.Sprintf("%s/ip=%s", viper.GetString("app.apitest"),ipAddr)
-	}else{
-		api = fmt.Sprintf("%s/ip=%s", viper.GetString("app.api"),ipAddr)
+	// currentTime := time.Now()
+	// c:=fmt.Sprintf("Current Time in String: %s", currentTime.String())
+	f, err := os.OpenFile("start.log", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		log.Println(err)
 	}
-	url:=viper.GetString("app.url")
-    result,err :=getApi(api)
-    if err !=nil{
-        os.Exit(1)
-    }
-    if !result.Status {
-        fmt.Println("status is false")
-        os.Exit(1)
-    }
-	openUrl(url)
+	defer f.Close()
+	logger := log.New(f, "", log.LstdFlags)
+	logger.Println(" start finish")
+	ipAddr := getIp()
+	mac, err := getMacAddr()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(mac)
+	mode := viper.GetString("app.mode")
+	var api string
+	if mode == "developement" {
+		api = fmt.Sprintf("%s/ip=%s&mac=%s", viper.GetString("app.apitest"), ipAddr, mac)
+	} else {
+		api = fmt.Sprintf("%s/ip=%s&mac=%s", viper.GetString("app.api"), ipAddr, mac)
+	}
+	url := viper.GetString("app.url")
+	result, err := getApi(api)
+	if err != nil {
+		os.Exit(1)
+	}
+	if !result.Status {
+		fmt.Println("status is false")
+		os.Exit(1)
+	}
+	openUrl(fmt.Sprintf(""+" %s&mac=%s",url,mac))
 }
-func openUrl(url string){
-    var cmd *exec.Cmd
+func openUrl(url string) {
+	// fmt.Println(url)
+	var cmd *exec.Cmd
 	switch runtime.GOOS {
 	case "windows":
 		cmd = exec.Command("cmd", "/c", "start", "chrome", url)
@@ -64,8 +83,8 @@ func openUrl(url string){
 		os.Exit(1)
 	}
 }
-func getIp() string{
-    // Get request
+func getIp() string {
+	// Get request
 	host, _ := os.Hostname()
 	addrs, _ := net.LookupIP(host)
 	var ipAddr string
@@ -77,29 +96,45 @@ func getIp() string{
 			break
 		}
 	}
-    return ipAddr
+	return ipAddr
 }
-func getApi(url string) (Response,error) {
-    client := http.Client{
-        Timeout: 5 * time.Second,
-    }
+func getApi(url string) (Response, error) {
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
 	resp, err := client.Get(url)
 	if err != nil {
 		fmt.Println("No response from request")
-		return Response{},err
+		return Response{}, err
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body) // response body is []byte
 	if err != nil {
 		fmt.Println("Can not get Response")
-		return Response{},err
+		return Response{}, err
 	}
 	var result Response
 	if err := json.Unmarshal(body, &result); err != nil { // Parse []byte to the go struct pointer
 		fmt.Println("Can not unmarshal JSON")
-		return Response{},err
+		return Response{}, err
 	}
 	fmt.Println(result)
-    return result,nil
+	return result, nil
 
+}
+
+func getMacAddr() (string, error) {
+	ifas, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	var as string
+	for _, ifa := range ifas {
+		a := ifa.HardwareAddr.String()
+		if a != "" {
+			as = a
+			break
+		}
+	}
+	return as, nil
 }
